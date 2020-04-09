973a44548bb5f10b96efc3a226ec44ebfdd91c55
Andrew Crozier <wacrozier@gmail.com>
2020-04-09 18:49:02 +0100

    Extra details in setup.py and version numbering

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
+from pathlib import Path
+
+
+README = Path(__file__).parent / "README.rst"
+
 
 setup(
     name="faculty-sphinx-theme",
+    description="A Sphinx theme for Faculty projects.",
+    long_description=README.read_text(),
+    author="Faculty",
+    author_email="opensource@faculty.ai",
+    license="Apache Software License",
     packages=["faculty_sphinx_theme"],
+    use_scm_version={"version_scheme": "post-release"},
+    setup_requires=["setuptools_scm"],
     install_requires=["sphinx_rtd_theme"],
     package_data={
         "faculty_sphinx_theme": [
